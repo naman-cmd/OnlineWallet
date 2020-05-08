6c47cfa4473aec83a9c425b303e972ce92181728
naman-cmd <61539916+naman-cmd@users.noreply.github.com>
2020-05-08 09:50:21 +0530

    Add files via upload

---
+++ E-Wallet-2/src/main/java/services/TransactionServices.java
@@ -0,0 +1,16 @@
+package services;
+
+import java.util.List;
+
+import entity.WalletTransactions;
+
+public interface TransactionServices {
+
+
+
+    WalletTransactions save(WalletTransactions transaction);
+    WalletTransactions findById(long id);
+    List<WalletTransactions> fetchAll();
+
+
+}
